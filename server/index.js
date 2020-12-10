c72d6bf1d3f1158f7c2838357265924ab4e50faf
umasahani <64558256+umasahani@users.noreply.github.com>
2020-12-09 22:01:58 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const calculateOrderAmount = items => {
   // Replace this constant with a calculation of the order's amount
   // Calculate the order total on the server to prevent
   // people from directly manipulating the amount on the client
-  return 1000;
+  return 1200;
 };
 
 app.get('/api/greeting', (req, res) => {
